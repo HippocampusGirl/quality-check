a6f4d1f3cae8c79b0d144afa688775d981492733
Lea Waller <lea@lea.science>
2024-05-25 09:44:43 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config = {
+          allowUnfree = true;
+          cudaSupport = true;
+        };
+      };
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [ pkgs.micromamba pkgs.ruff ];
+        shellHook = ''
+          export PYTHONBREAKPOINT="ipdb.set_trace"
+          export PYTHONDONTWRITEBYTECODE="1"
+          export PYTHONUNBUFFERED="1"
+          export PYTHONPATH="$(git rev-parse --show-toplevel)/src"
+          eval "$(micromamba shell hook --shell=posix)"
+          micromamba activate "machine-learning"
+        '';
+      };
+    };
+}
